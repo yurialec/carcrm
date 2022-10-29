3b5d287f285dcc960bfe7e03028ad524a30fe46e
Yuri Alec <yurialec@hotmail.com>
2022-10-29 18:04:57 -0300

    dev-02

--- frontend/src/Rotas.js
+++ frontend/src/Rotas.js
@@ -1,14 +1,14 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { CircularProgress } from "@mui/material";
 
 const Rotas = () => {
     <Suspense fallback={<div className="d-flex justify-content-center mt-5 pt-5"> <CircularProgress /> </div>}>
     <Router>
-        <Switch>
+        <Routes>
             <Route path="/cadastro" component={() => (<h1>Cadastro</h1>)} />
             <Route path="/login" component={() => (<h1>Login</h1>)} />
-        </Switch>
+        </Routes>
     </Router>
     </Suspense>
 }
