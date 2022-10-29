81e606a98a13b98cf05db5e0ec3eea074060fd80
Yuri Alec <yurialec@hotmail.com>
2022-10-29 18:27:57 -0300

    Corrigindo conflitos

--- frontend/src/Rotas.js
+++ frontend/src/Rotas.js
@@ -1,12 +1,14 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import { CircularProgress } from "@mui/material";
+import Cadastro from "./pages/Cadastro";
+
 
 const Rotas = () => {
     <Suspense fallback={<div className="d-flex justify-content-center mt-5 pt-5"> <CircularProgress /> </div>}>
     <Router>
         <Routes>
-            <Route path="/cadastro" component={() => (<h1>Cadastro</h1>)} />
+            <Route path="/cadastro" component={Cadastro} />
             <Route path="/login" component={() => (<h1>Login</h1>)} />
         </Routes>
     </Router>
---
+++ frontend/src/pages/Cadastro.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cadastro = () => {
+    <h1>Cadastro</h1>
+}
+
+export default Cadastro;
